62e8b406231e98ddbdf8f501fbc04d228072483b
Arun Frey <70369657+ArunFrey@users.noreply.github.com>
2022-09-29 00:32:55 +0100

    add race data

---
+++ get_races.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import argparse
+import pandas as pd 
+from helpers import format_data
+
+
+def get_raw_races(username, last_races = 99999999): 
+
+    """Downloads a user's typeracer data from typeracerdata.com
+    
+
+    Args:
+        username (str): Username used for typeracer.
+        last_races (int, optional): Number of last races to retrieve. Defaults to 99999999
+
+    Returns:
+        pd.DataFrame: A dataframe containing a user's race data. 
+    """
+
+    # get parsed data
+    r = requests.get(f"http://www.typeracerdata.com/profile?username={username}&last={last_races}")
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+    
+    # extract data from tables
+    tables = soup.find_all('table')
+    table = tables[2]
+    columns = [header.text for header in table.find_all("th")]
+    
+    race_data = list()
+    for row in table.find_all("tr"): 
+        content = list()
+        for column in row.find_all("td"): 
+            if len(column) > 0:
+                content.append(column.text)
+        if len(content) > 0:
+            race_data.append(content)
+
+    df = pd.DataFrame(race_data, columns = columns)
+    
+    return df
+
+
+def main():
+    
+    """Main function to be executed directly from terminal
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Getting racer data from typeracer.')
+    parser.add_argument('-u', '--user_name', type=str,
+                        required=True, help='Enter the username of the user whose data you want to download.')
+    parser.add_argument('-lr', '--last_races', type=int, required=False,
+                        help='Enter the last number of races you wish to download. Defaults to all.')
+    args = parser.parse_args()
+    
+    # get data
+    df = get_raw_races(args.user_name, args.last_races)
+    # format data
+    df = format_data(df)
+    # store data
+    df.to_csv(f"data/races_{args.user_name}.csv")
+
+if __name__ == "__main__":
+    main()
