e7f62a67bcd745428dbeb48156ecaac070dca168
Arun Frey <70369657+ArunFrey@users.noreply.github.com>
2022-10-03 09:47:58 +0100

    added main() to visualiser

--- get_races.py
+++ get_races.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
 import argparse
 import pandas as pd 
 from helpers import format_data
--- visualizer.py
+++ visualizer.py
@@ -1,20 +1,17 @@
 from get_races import get_raw_races
 from get_texts import get_raw_texts
 from helpers import format_data
-from os.path import exists
 
+from plotly.subplots import make_subplots
 
 import logging
+import argparse
 import numpy as np
 import pandas as pd
 import plotly.io as pio
 import plotly.express as px
-import plotly.graph_objs as go
+
 pio.templates.default = "plotly_white"
-
-from string import punctuation
-
-count = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
 
 
 def plot_wpm_over_time(username, rolling_avg = 50):
@@ -54,6 +51,15 @@ def plot_wpm_over_time(username, rolling_avg = 50):
 
 
 def plot_hist(username, min_races = 100):
+    """Plots a histogram of average text speed, and adds vlines for user performance (avg, last 10m, max)
+
+    Args:
+        username (str): Username of user for which plot should be created
+        min_races (int, optional): Cutoff for including only races with at least x amount of races. Defaults to 100.
+
+    Returns:
+        Plotly plot
+    """
     
     # get user data
     try: 
@@ -82,15 +88,15 @@ def plot_hist(username, min_races = 100):
 
     fig.add_vline(x = np.mean(df['wpm']), 
                   line_dash = "dash",
-                  annotation_text = int(np.mean(df['wpm'])))
+                  annotation_text = f"Avg: {int(np.mean(df['wpm']))}")
 
-    fig.add_vline(x = np.mean(df['wpm'][0:10]), 
+    fig.add_vline(x = np.mean(df['wpm'][0:20]), 
                   line_dash = "dot",
-                  annotation_text = int(np.mean(df['wpm'][0:10])))
+                  annotation_text = f"Last 10: {int(np.mean(df['wpm'][0:10]))}")
 
     fig.add_vline(x = np.max(df['wpm']), 
                   line_dash = "longdash",
-                  annotation_text = int(np.max(df['wpm'])))
+                  annotation_text = f"Max: {int(np.max(df['wpm']))}")
     
     fig.write_image(f"plots/{username}_hist.pdf", width=300, height=350)
     
@@ -99,3 +105,27 @@ def plot_hist(username, min_races = 100):
 
 def main(): 
     
+    parser = argparse.ArgumentParser(
+        description='Plotting data for typeracer user.')
+    parser.add_argument('-u', '--user_name', type=str,
+                        required=True, help='Enter the username of the user whose data you want to plot.')
+    args = parser.parse_args()
+
+    fig = make_subplots(rows=1, cols=2)
+    
+    figures = [
+        plot_wpm_over_time(args.user_name),
+        plot_hist(args.user_name)
+    ]
+
+    fig = make_subplots(rows=len(figures), cols=1) 
+
+    for i, figure in enumerate(figures):
+        for trace in range(len(figure["data"])):
+            fig.append_trace(figure["data"][trace], row=i+1, col=1)
+    
+    return fig.show()
+
+
+if __name__ == "__main__":
+    main()
