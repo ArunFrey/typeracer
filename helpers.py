a8f6e4e184241000fb21fd6e2d37b5c1544ef66a
Arun Frey <70369657+ArunFrey@users.noreply.github.com>
2022-10-10 15:55:30 +0100

    added residual plot

--- helpers.py
+++ helpers.py
@@ -1,4 +1,10 @@
+from curses.ascii import isupper
+import string
 import pandas as pd
+import numpy as np 
+
+from sklearn.linear_model import LinearRegression
+from plotly.subplots import make_subplots
 
 
 def format_data(df):
@@ -48,4 +54,40 @@ def format_data(df):
             except:
                 print(f"Column {c} cannot be converted to numeric")
 
+    return df
+
+
+def combine_text_and_races(df, texts, texts_abbrev):
+    """Merges user and text data
+
+    Args:
+        df (pd.DataFrame): User data
+        texts (pd.DataFrame): Text data
+        texts_abbrev (pd.DataFrame): Text data (abbreviated)
+
+    Returns:
+        pd.DataFrame: Merged dataframe
+    """
+
+    # merge data
+    df = pd.merge(df, texts_abbrev[["id", "text"]], on="text", how="left")
+    df = df.rename(columns={"text": "text_abbrev"})
+    df = pd.merge(df, texts, on="id", how="left")
+    
+    count = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
+    
+    # add punctuation, letter, and capitalization count
+    df['letters'] =  [count(words, string.ascii_letters) for words in df['text']]
+    df['punct'] = [count(words, string.punctuation) for words in df['text']]
+    df['punct_rate'] = df['punct']/df['length']
+    df['cap'] = [sum(1 for c in words if c.isupper()) for words in df['text']]
+    df['cap_rate'] = df['cap']/df['length']
+
+    # control for linear progression, and save residual
+    X = np.array(df['race']).reshape((-1, 1))
+    y = np.array(df['wpm'])
+    reg = LinearRegression().fit(X, y)
+    df['pred'] = reg.predict(X)
+    df['res'] = (y - df['pred'])
+    
     return df
--- visualizer.py
+++ visualizer.py
@@ -1,40 +1,18 @@
-from get_races import get_raw_races
-from get_texts import get_raw_texts
-from helpers import format_data
+from helpers import combine_text_and_races
 
 from plotly.subplots import make_subplots
 
-import logging
-import argparse
 import numpy as np
-import pandas as pd
 import plotly.io as pio
 import plotly.express as px
 
 pio.templates.default = "plotly_white"
 
 
-def plot_wpm_over_time(username, rolling_avg=50):
-    """Generates a plotly plot of words/minute over time
-
-    Args:
-        username (str): Username used for race data
-        rolling_avg (int, optional): Number of races aggregated in rolling average. Defaults to 50.
-
-    Returns:
-        obj: plotly figure
-    """
-    try:
-        df = pd.read_csv(f"data/races_{username}.csv")
-    except:
-        logging.warning(
-            f"User data for {username} not yet available. Downloading now..."
-        )
-        df = get_raw_races(username)
-        df = format_data(df)
+def plot_wpm_over_time(user, rolling_avg=50):
 
     fig = px.scatter(
-        df,
+        user,
         x="race",
         y="wpm",
         color="acc",
@@ -48,38 +26,10 @@ def plot_wpm_over_time(username, rolling_avg=50):
         },
     )
 
-    fig.write_image(f"plots/{username}_wpm-time.png", width=1200, height=600, scale = 1)
-
     return fig
 
 
-def plot_hist(username, min_races=100):
-    """Plots a histogram of average text speed, and adds vlines for user performance (avg, last 10m, max)
-
-    Args:
-        username (str): Username of user for which plot should be created
-        min_races (int, optional): Cutoff for including only races with at least x amount of races. Defaults to 100.
-
-    Returns:
-        Plotly plot
-    """
-
-    # get user data
-    try:
-        df = pd.read_csv(f"data/races_{username}.csv")
-    except:
-        logging.warning(
-            f"User data for {username} not yet available. Downloading now..."
-        )
-        df = get_raw_races(username)
-        df = format_data(df)
-
-    # get text data
-    try:
-        texts = pd.read_csv(f"data/texts.csv")
-        texts_abbrev = pd.read_csv(f"data/texts_abbrev.csv")
-    except:
-        logging.error(f"Text data not yet available. Needs to be downloaded first")
+def plot_hist(user, texts, min_races=100):
 
     # select races with at least min_races
     texts = texts[texts["races"] >= min_races]
@@ -87,59 +37,66 @@ def plot_hist(username, min_races=100):
     fig = px.histogram(
         texts,
         x="average",
-        opacity=0.8,
+        histnorm='probability',
+        color_discrete_sequence=["rgb(255, 127, 14)"],
         labels={
             "average": "Average performance across races",
         },
     )
 
     fig.add_vline(
-        x=np.mean(df["wpm"]),
+        x=np.mean(user["wpm"]),
         line_dash="dash",
-        annotation_text=f"Avg: {int(np.mean(df['wpm']))}",
     )
 
     fig.add_vline(
-        x=np.mean(df["wpm"][0:20]),
+        x=np.mean(user["wpm"][0:20]),
         line_dash="dot",
-        annotation_text=f"Last 10: {int(np.mean(df['wpm'][0:10]))}",
     )
 
     fig.add_vline(
-        x=np.max(df["wpm"]),
+        x=np.max(user["wpm"]),
         line_dash="longdash",
-        annotation_text=f"Max: {int(np.max(df['wpm']))}",
     )
-
-    fig.write_image(f"plots/{username}_hist.png", width=1000, height=700, scale = 1)
 
     return fig
 
 
-def main():
+def plot_facetted_residuals(user,
+                           texts,
+                           texts_abbrev, 
+                           y = 'res', 
+                           vars = ['punct_rate', 'cap_rate', 'difficulty_rating', 'length'], 
+                           vars_labels = ['Punctuation rate', 'Capitalization rate', 'Difficulty', 'Length'], 
+                           n_cols = 2):
 
-    parser = argparse.ArgumentParser(description="Plotting data for typeracer user.")
-    parser.add_argument(
-        "-u",
-        "--user_name",
-        type=str,
-        required=True,
-        help="Enter the username of the user whose data you want to plot.",
-    )
-    args = parser.parse_args()
+    # format data
+    df = combine_text_and_races(user, texts, texts_abbrev)    
+        
+    # plot data    
+    n_rows = -(-len(vars) // n_cols) 
+    
+    row_pos, col_pos = 1, 0
+    
+    fig = make_subplots(rows=n_rows, 
+                        cols=n_cols,
+                        subplot_titles=vars_labels)
 
-    fig = make_subplots(rows=1, cols=2)
-
-    figures = [plot_wpm_over_time(args.user_name), plot_hist(args.user_name)]
-
-    fig = make_subplots(rows=len(figures), cols=1)
-
-    for i, figure in enumerate(figures):
-        for trace in range(len(figure["data"])):
-            fig.append_trace(figure["data"][trace], row=i + 1, col=1)
-
-    return fig.show()
-
-
-if __name__ == "__main__":
-    main()
+    for v in vars:
+        # trace extracted from the fig
+        trace = px.scatter(df, 
+                           x=v, 
+                           y=y, 
+                           color = 'acc',
+                           opacity = 0.5,
+                           trendline='lowess', 
+                           trendline_color_override='#DC143C')["data"]
+        
+        # auto selecting a position of the grid
+        if col_pos == n_cols: row_pos += 1
+        col_pos = col_pos + 1 if (col_pos < n_cols) else 1
+        # adding trace to the grid
+        fig.add_trace(trace[0], row=row_pos, col=col_pos)
+        fig.add_trace(trace[1], row=row_pos, col=col_pos)
+        
+    return fig
